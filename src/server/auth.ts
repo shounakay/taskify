6b9aaf09ffc5f0614e4d60f6d6edb8022fd6bac3
Shounak Chavan <chavanshounak24@gmail.com>
2024-01-25 00:00:23 +0530

    fix: Incorrect import

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -7,16 +7,8 @@ import {
 import CredentialsProvider from "next-auth/providers/credentials";
 
 import { db } from "@/server/db";
-import async from "../app/pages";
 import { compare } from "bcrypt";
-import { signinSchema } from "../app/_components/Signin";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: {
@@ -26,17 +18,6 @@ declare module "next-auth" {
     } & DefaultSession["user"];
   }
 
-  // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
-  // }
-}
-
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authOptions: NextAuthOptions = {
   callbacks: {
     session: ({ session, user, token, ...rest }) => {
