f0498f655ec51c79e6cc8d9d4cad8f705f5aebea
Shounak Chavan <chavanshounak24@gmail.com>
2024-02-18 14:57:25 +0530

    chore: Refactor

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -93,9 +93,4 @@ export const authOptions: NextAuthOptions = {
   ],
 };
 
-/**
- * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
- *
- * @see https://next-auth.js.org/configuration/nextjs
- */
 export const getServerAuthSession = () => getServerSession(authOptions);
